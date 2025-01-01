4a2d87c2ebc6e96541f090300d474b2abaf33bc4
shadabBhai <iamshadabkhan3@github.com>
2025-01-01 21:07:22 +0530

    remove code

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const bookingRoutes = require("./routes/booking");
-require("dotenv").config();
 
 const app = express();
 const PORT = 3000;
